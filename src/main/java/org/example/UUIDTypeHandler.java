71054d34cfc27c29fbdf9d7781875a78c692330f
nyampom <tubutubuu@gmail.com>
2024-07-16 21:23:33 +0900

    none

---
+++ src/main/java/org/example/UUIDTypeHandler.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+
+import java.sql.*;
+import java.util.UUID;
+
+public class UUIDTypeHandler extends BaseTypeHandler<UUID> {
+
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, UUID parameter, JdbcType jdbcType) throws SQLException {
+        ps.setObject(i, parameter);
+    }
+
+    @Override
+    public UUID getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        return (UUID) rs.getObject(columnName);
+    }
+
+    @Override
+    public UUID getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        return (UUID) rs.getObject(columnIndex);
+    }
+
+    @Override
+    public UUID getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        return (UUID) cs.getObject(columnIndex);
+    }
+}
